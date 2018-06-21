e8f8b899cba1138e9ee0b1e3522393f8a983282d
Kanishka Rao <kanishka.palak@gmail.com>
2018-06-21 21:13:04 +0530

    Create C++ Class Templates.cpp

---
+++ C++ Class Templates.cpp
@@ -0,0 +1,9 @@
+/*Write the class AddElements here*/
+template <class T>
+class AddElements {
+T element;
+public:
+AddElements (T arg) {element=arg;}
+T add (T arg) {return (element+arg);}
+T concatenate (T arg) {return (element+arg);}
+};
